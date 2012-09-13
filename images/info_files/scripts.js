da376f7d95978644162590bec20cd89b4e4ea778
Harish <harish@fueled.co>
2012-09-13 11:26:34 +0530

    init

---
+++ images/info_files/scripts.js
@@ -0,0 +1,48 @@
+function naviFirst(){
+	$(".info").fadeIn();
+	$(".cntct").fadeIn();
+	$(".info").animate({top:"55px"});
+	$(".cntct").animate({top:"55px"});
+	$(".info").animate({top:"35px"});
+	$(".cntct").animate({top:"35px"});
+	$(".info").animate({top:"40px"});
+	$(".cntct").animate({top:"40px"});
+}
+function naviLater(){
+	$(".home").fadeIn();
+	$(".cmpny").fadeIn();
+	$(".clnts").fadeIn();
+	$(".home").animate({top:"55px"});
+	$(".cmpny").animate({top:"55px"});
+	$(".clnts").animate({top:"55px"});
+	$(".home").animate({top:"35px"});
+	$(".cmpny").animate({top:"35px"});
+	$(".clnts").animate({top:"35px"});
+	$(".home").animate({top:"40px"});
+	$(".cmpny").animate({top:"40px"});
+	$(".clnts").animate({top:"40px"});
+}
+function animateFooter(){
+	$(".footer").fadeIn();
+	$(".footer").animate({top:"97%"});
+}
+function animateFirstLine(){
+	$(".leftbox .c").fadeIn();
+	$(".leftbox .c").animate({marginTop:"0"},1100);
+	$(".leftbox .o").fadeIn();
+	$(".leftbox .o").animate({marginTop:"-200px"},1100);
+	$(".leftbox .n").fadeIn();
+	$(".leftbox .n").animate({marginTop:"-200px"},1100);
+}
+function animateSecondLine(){
+	$(".leftbox .t").fadeIn();
+	$(".leftbox .t").animate({marginTop:"0"},1100);
+	$(".leftbox .a").fadeIn();
+	$(".leftbox .a").animate({marginTop:"-177px"},1100);
+
+	$(".leftbox .c1").fadeIn();
+	$(".leftbox .c1").animate({marginTop:"-177px"},1100);
+	$(".leftbox .t1").fadeIn();
+
+	$(".leftbox .t1").animate({marginTop:"-177px"},1100);
+}

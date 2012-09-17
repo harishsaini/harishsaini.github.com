3779a938206aee199148f3305dda7d89bdf10912
Harish <harish@fueled.co>
2012-09-17 14:40:17 +0530

    height info

---
+++ Projects/lnoir/scripts/footer.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<body>
+<div class="footer" style="display:none;">
+	Copyright &#169; 2010.All Rights Reserved
+</div>
+</body>
---
+++ Projects/lnoir/scripts/header.php
@@ -0,0 +1,15 @@
+<?php
+echo 'hello';
+?>
+<!DOCTYPE html>
+<body>
+<div class="navi">
+	<ul>
+		<li><a class="home" href="../lnoir.html">Home</a></li>
+		<li><a class="info" href="info.html">info</a></li>
+		<li><a class="cmpny" href="company.html">company</a></li>
+		<li><a class="cntct" href="contact.html">contact</a></li>
+		<li><a class="clnts" href="#" style="cursor: default;">clients</a></li>
+	</ul>
+</div>
+</body>
